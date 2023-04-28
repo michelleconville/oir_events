9f282ad93595183cd90ee0e58c57065bad294d57
michelleconville <michelle.conville@gmail.com>
2023-04-28 16:59:48 +0000

    Create add_event view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Event
 
-# Create your views here.
+
+class AddEvent(CreateView):
+    """ Add event view """
+    template_name = 'events/add_event.html'
+    model = Event
+    success_url = '/events/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddEvent, self).form_valid(form)
