537c571292ac0088336d0fb65002c3d07c5899e2
michelleconville <michelle.conville@gmail.com>
2023-04-28 16:01:02 +0000

    Create model to create and manage events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from djrichtextfield.models import RichTextField
+from django_resized import ResizedImageField
+
+
+class Event(models.Model):
+    """
+    A model to create and manage events
+    """
+    user = models.ForeignKey(User, related_name='event_owner', on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, null=False, blank=False)
+    summary = models.CharField(max_length=300, null=False, blank=False)
+    description = RichTextField(max_length=10000, null=False, blank=False)
+    image = ResizedImageField(
+        size=[400, None],
+        quality=75,
+        upload_to="events/",
+        force_format="WEBP",
+        blank=False,
+        null=False,
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    date = models.DateField(default=return_date)
+    active = models.BooleanField(default=False)
+    posted_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-posted_date"]
+
+    def __str__(self):
+        return str(self.title)
