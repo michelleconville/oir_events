3a61c214415e3d8d53d3395002406ad1947fb172
michelleconville <michelle.conville@gmail.com>
2023-04-30 14:28:00 +0000

    Add basic style to cards on events.hml page

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import AddEvent
+from .views import AddEvent, Events
 
 
 urlpatterns = [
     path("add/", AddEvent.as_view(), name="add_event"),
     path("events/", Events.as_view(), name="events"),
-
 ]
