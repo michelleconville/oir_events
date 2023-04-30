82a36962371faad220f9296821d05bd4f2090a1d
michelleconville <michelle.conville@gmail.com>
2023-04-30 16:07:56 +0000

    Create DeleteEvent class

--- events/views.py
+++ events/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView
+
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin
+)
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -15,7 +19,7 @@ class Events(ListView):
 
 
 class EventDetail(DetailView):
-    """View a event"""
+    """View an event"""
 
     template_name = "events/event_detail.html"
     model = Event
@@ -33,3 +37,12 @@ class AddEvent(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddEvent, self).form_valid(form)
+
+
+class DeleteEvent(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Delete an Event"""
+    model = Event
+    success_url = "/events/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
