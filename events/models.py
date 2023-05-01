aa859c460ffbade1dbe17fc592df5c4a6a5cb5cd
michelleconville <michelle.conville@gmail.com>
2023-05-01 15:58:41 +0000

    Delete booking app

--- events/models.py
+++ events/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django import forms 
+from django import forms
 
 from djrichtextfield.models import RichTextField
 from django_resized import ResizedImageField
