d9572641e4f495f37a98c9242084e15a4a90f318
michelleconville <michelle.conville@gmail.com>
2023-05-01 20:12:51 +0000

    Create model for contact form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    first_name = models.CharField(max_length=254, null=True, blank=True)
+    last_name = models.CharField(max_length=254, null=True, blank=True)
+    phone_number = models.CharField(max_length=16, null=True, blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
