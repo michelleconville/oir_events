d3a33f17f5504d3142a12b7cc32e37e91dd6eed2
michelleconville <michelle.conville@gmail.com>
2023-05-07 19:50:19 +0000

    Create an edit booking view and url

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import CreateBooking, UserBookings, StaffBookings, DeleteBooking
+from .views import (
+    CreateBooking, UserBookings,
+    StaffBookings, EditBooking,
+    DeleteBooking
+)
 
 
 urlpatterns = [
     path("add/", CreateBooking.as_view(), name="create_booking"),
     path("user_bookings/", UserBookings.as_view(), name="user_bookings"),
     path("staff_bookings/", StaffBookings.as_view(), name="staff_bookings"),
-    path("delete/<slug:pk>/", DeleteBooking.as_view(), name="delete_booking"),
-
+    path('bookings/<int:pk>/edit/', EditBooking.as_view(), name='edit_booking'),
+    path('bookings/<int:pk>/delete/', DeleteBooking.as_view(), name='delete_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DeleteView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # from django.shortcuts import render
@@ -49,6 +49,26 @@ class StaffBookings(LoginRequiredMixin, UserPassesTestMixin, ListView):
 
     def test_func(self):
         return self.request.user.is_staff
+
+
+class EditBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    A view to edit a booking
+    """
+    template_name = "bookings/edit_booking.html"
+    model = Booking
+    form_class = BookingForm
+    success_url = reverse_lazy("staff_bookings")
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            'Booking successfully updated'
+        )
+        return super().form_valid(form)
 
 
 class DeleteBooking(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
